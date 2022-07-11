e6255ffbe1e2fe79c3da0d1414b16c642a639f64
sashakojadinovic <stanojeglavas@msn.com>
2022-07-11 20:42:19 +0200

    Homepage

--- routes/web.php
+++ routes/web.php
@@ -49,7 +49,8 @@ Auth::routes([
     'register'=>false
 ]);
 
-Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+//::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+Route::get('/katalog',[App\Http\Controllers\HomeController::class, 'index'])->name('home');
 Route::resource('categories', CategoryController::class)->middleware('auth');
 //Route::resource('categories', CategoryController::class)->only(['index','show']);
 
